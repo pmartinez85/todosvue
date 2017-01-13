a6b7ff42fa4d267d141af2347949c7c6aa255a4a
Pedro Martinez <pmartinez1085@gmail.com>
2017-01-13 16:10:06 +0100

    corregir doble intro

--- src/main.js
+++ src/main.js
@@ -4,10 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
-
-
 Vue.use(VueMaterial)
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
