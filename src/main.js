73bf709dad0a7da63cb5f7626a5da0d10fd62def
Pedro Martinez <pmartinez1085@gmail.com>
2017-01-13 16:06:35 +0100

    installing material

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
 
 /* eslint-disable no-new */
 new Vue({
