f6bbaccb4744e996563edea0c0e2c643c453057a
Pedro Martinez <pmartinez1085@gmail.com>
2017-01-13 16:07:49 +0100

    add vuematerial

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import App from './App'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
 
+
+Vue.use(VueMaterial)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
